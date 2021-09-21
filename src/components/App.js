491f9288d96ef81b13c6cefc27a56e824e3d0a02
Muhammad <muhdibee@gmail.com>
2021-09-21 21:32:50 +0100

    Loading

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './DashBoard';
+import LoadingBar from 'react-redux-loading';
 
 class App extends Component {
 
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
         <div>
-          <h1>Starter Code</h1>
+          <LoadingBar />
           {this.props.loading === true ? null: <Dashboard/>}
         </div>
     )
