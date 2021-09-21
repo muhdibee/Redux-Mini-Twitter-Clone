fab95564a3c71dbcf998ac483834f3b184cfbf1d
Muhammad <muhdibee@gmail.com>
2021-09-21 23:21:32 +0100

    Like tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,34 @@
+import { _saveLikeToggle } from "../utils/_DATA";
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
+export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+
 
 export const receiveTweets = (tweets) => {
     return {
         type: RECEIVE_TWEETS,
         tweets,
+    }
+}
+
+function toggleTweet ({id, authedUser, hasLiked}) {
+    return {
+        type: TOGGLE_TWEET,
+        id,
+        authedUser,
+        hasLiked,
+    }
+
+}
+
+export function handleToggleTweet(info) {
+    return (dispatch) => {
+        dispatch(toggleTweet(info))
+
+        return _saveLikeToggle(info)
+        .catch((e) => {
+            console.warn('Error in handleToggleTweet: ', e)
+            dispatch(toggleTweet(info))
+            alert('There was an error liking the tweet. Try again.')
+        })
     }
 }
--- src/components/tweet.js
+++ src/components/tweet.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {formatTweet, formatDate} from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti/index'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
     handleLike = (e) => {
       e.preventDefault()
+      const { dispatch, tweet, authedUser } = this.props
 
-      // todo: Handle Like Tweet
+      dispatch(handleToggleTweet({id: tweet.id, hasLiked: tweet.hasLiked, authedUser}))
     }
+
     toParent = (e, id) => {
       e.preventDefault()
       // todo: Redirect to parent Tweet.
