c4ebcad5076de69e6df2be889acd5d913a6adab4
Muhammad <muhdibee@gmail.com>
2021-09-22 13:17:08 +0100

    Tweet Page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from '../actions/shared';
 import Dashboard from './DashBoard';
 import LoadingBar from 'react-redux-loading';
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
 
@@ -16,9 +17,10 @@ class App extends Component {
         <div>
           <LoadingBar />
           {this.props.loading === true ? null
-          // : <Dashboard/>
-          : <NewTweet/>
-          }
+          : <Dashboard/>
+          // : <NewTweet/>
+          // : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>
+        }
         </div>
     )
   }
